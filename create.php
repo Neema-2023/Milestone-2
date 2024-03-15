e11c7947b59e372308bbd538ed190315aa4da373
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-03-15 13:20:10 -0400

    Update create.php

--- create.php
+++ create.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
 
-    $sql = "INSERT INTO users (name, email) VALUES (:name, :email)";
+    $sql = "INSERT INTO Employees (name, email) VALUES (:name, :email)";
     $stmt= $pdo->prepare($sql);
     $stmt->execute([$name, $email]);
 
@@ -16,14 +16,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Add User</title>
+    <title>Add Employee</title>
     <link  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div class="container">
-    <h2>Add User</h2>
-    <form method="POST" action="add_user.php">
+    <h2>Add Employee</h2>
+    <form method="POST" action="add_employee.php">
         <div class="form-group">
             <label for="name">Name:</label>
             <input type="text" name="name" id="name" class="form-control" required>
