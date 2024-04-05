db174ffaa794a178c71b8b547e0744392757f94e
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-04 21:16:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,43 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>CST323 HRMS</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <div class="container mt-3"><h2>Training Schedule</h2>
-<a href="home.php" class="btn btn-primary mb-2">Go Home</a>
+</head>
+<body>
+    <div class="container mt-3">
+        <h2>Training Schedule</h2>
+        
+        <a href="home.php" class="btn btn-primary mb-2">Go Home</a>
+        <a href="create.php" class="btn btn-primary mb-2">Add New Training</a>
 
-<a href="create.php" class="btn btn-primary mb-2">Add New Training</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Employee Name</th>
+                    <th>Training Class</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Assuming $trainings is predefined somewhere above this code or is fetched from a database.
+                foreach ($trainings as $training) {
+                    echo "<tr>
+                              <td>{$training['employeeName']}</td>
+                              <td>{$training['trainingClass']}</td>
+                              <td>{$training['trainingDate']}</td>
+                              <td>
+                                  <a href='update.php?id={$training['id']}' class='btn btn-warning'>Edit</a>
+                                  <a href='delete.php?id={$training['id']}' class='btn btn-danger'>Delete</a>
+                              </td>
+                          </tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
 
-<table class="table"><thead><tr><th>Employee Name</th><th>Training Class</th><th>Date</th><th>Actions</th></tr></thead><tbody>
-
-foreach ($trainings as $training) {
-     "<tr><td>{$training['employeeName']}</td><td>{$training['trainingClass']}</td><td>{$training['trainingDate']}</td>
-          <td><a href='update.php?id={$training['id']}' class='btn btn-warning'>Edit</a>
-          <a href='delete.php?id={$training['id']}' class='btn btn-danger'>Delete</a></td></tr>";
-}
-
-</tbody></table></div>
-?>
