c7236b0bc40fb39a00d994bfb6396b7b5a935ace
Neema-2023 <131406712+Neema-2023@users.noreply.github.com>
2024-04-04 21:28:42 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,18 +1,19 @@
-<?php
-// change home.php to index.php?
-//require_once __DIR__ . '/../config/bootstrap.php';
-// require_once __DIR__ . '/../vendor/autoload.php';
-$logger = require_once __DIR__ . '/../config/bootstrap.php';
-
-$logger->info('User accessed home.php');
-
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-<div class="container mt-5">
-<h1>Welcome to the CST323HR Management System</h1>
-<div class="list-group mt-3">
-<a href="employeemanagement.php" class="list-group-item list-group-item-action">Employee Management</a>
-<a href="departments.php" class="list-group-item list-group-item-action">Departments</a>
-<a href="reports.php" class="list-group-item list-group-item-action">Training Reports</a>
-<a href="index.php" class="list-group-item list-group-item-action">Schedule a Class</a>
-</div></div>'
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CST323 HR Management System</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Welcome to the CST323HR Management System</h1>
+        <div class="list-group mt-3">
+            <a href="employeemanagement.php" class="list-group-item list-group-item-action">Employee Management</a>
+            <a href="departments.php" class="list-group-item list-group-item-action">Departments</a>
+            <a href="reports.php" class="list-group-item list-group-item-action">Training Reports</a>
+            <a href="index.php" class="list-group-item list-group-item-action">Schedule a Class</a>
+        </div>
+    </div>
+</body>
+</html>
